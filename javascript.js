b2d51b2ad083ec040cc6f2372cbe0cf151063c1e
jesse <jesse@legion.home>
2022-03-17 13:09:25 +0200

    Made changes to Javascript
    Drawing grid is now done in a own function, which clears precvious grids
    if they exist.
    
    Clear button now calls for this new createGrid function.

--- javascript.js
+++ javascript.js
@@ -1,34 +1,54 @@
 const grid = document.querySelector('.grid');
 const clear = document.querySelector(".clear-btn");
 
-let gridSize = prompt("Give a grid size. For example 16. (that give 16x16");
+createGrid();
 
-let squareSize = 600 / gridSize;
-grid.style.gridTemplateRows = `repeat(${gridSize}, ${squareSize}px)`; 
-grid.style.gridTemplateColumns = `repeat(${gridSize}, ${squareSize}px)`; 
+function createGrid() {
 
+    clearGrid();
 
-for (let r = 1; r <= gridSize; r++) {
-    for (let c = 1; c <= gridSize; c++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.setAttribute('id', `${r}x${c}`);
-        square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
-        grid.appendChild(square);
+    let gridSize = prompt("Give a grid size. For example 16. (that give 16x16");
+    let squareSize = 600 / gridSize;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, ${squareSize}px)`;
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, ${squareSize}px)`;
+
+    for (let r = 1; r <= gridSize; r++) {
+        for (let c = 1; c <= gridSize; c++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.setAttribute('id', `${r}x${c}`);
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            grid.appendChild(square);
+        }
     }
+
+    for (square of document.querySelectorAll('.square')) {
+        square.addEventListener('mouseover', changeColor)
+    }
+    
+    function changeColor(e) {
+        e.target.style.backgroundColor = "green";
+    }
+
 }
 
-for (square of document.querySelectorAll('.square')){
-    square.addEventListener('mouseover', changeColor)
-}
 
-function changeColor(e){
-    e.target.style.backgroundColor = "green";
-}
 
-clear.addEventListener("click", () => { 
-    for (square of document.querySelectorAll('.square')){
+
+clear.addEventListener("click", () => {
+    for (square of document.querySelectorAll('.square')) {
         square.style.backgroundColor = "";
     }
- });
+    createGrid();
+});
+
+
+
+function clearGrid(){
+    if (grid.firstChild) {
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        }
+    } else return;
+}

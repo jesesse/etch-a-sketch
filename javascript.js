67a79a506c493f82c4a0b834c7d7f2c37d1c8031
jesse <jesse@legion.home>
2022-03-17 12:34:20 +0200

    Now the program creates a grid based on the user input. I draws fto the grid with mouse hovering on the squares. It clears the ddrawn picture.
    It still needs to create a whole new grid, based on the user input, when
    clear-button is pressed.

---
+++ javascript.js
@@ -0,0 +1,34 @@
+const grid = document.querySelector('.grid');
+const clear = document.querySelector(".clear-btn");
+
+let gridSize = prompt("Give a grid size. For example 16. (that give 16x16");
+
+let squareSize = 600 / gridSize;
+grid.style.gridTemplateRows = `repeat(${gridSize}, ${squareSize}px)`; 
+grid.style.gridTemplateColumns = `repeat(${gridSize}, ${squareSize}px)`; 
+
+
+for (let r = 1; r <= gridSize; r++) {
+    for (let c = 1; c <= gridSize; c++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.setAttribute('id', `${r}x${c}`);
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        grid.appendChild(square);
+    }
+}
+
+for (square of document.querySelectorAll('.square')){
+    square.addEventListener('mouseover', changeColor)
+}
+
+function changeColor(e){
+    e.target.style.backgroundColor = "green";
+}
+
+clear.addEventListener("click", () => { 
+    for (square of document.querySelectorAll('.square')){
+        square.style.backgroundColor = "";
+    }
+ });
